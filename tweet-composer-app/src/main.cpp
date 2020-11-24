653cb18ee804eb0c9f6f6e2c402679e910edfee2
chiposur <osurov314@gmail.com>
2020-11-23 22:12:01 -0800

    Encode all QString objects in UTF-8

--- tweet-composer-app/src/main.cpp
+++ tweet-composer-app/src/main.cpp
@@ -1,9 +1,14 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
+    // Encode QString text in UTF-8 (default is UTF-16)
+    QTextCodec *utf8Codec = QTextCodec::codecForName("UTF-8");
+    QTextCodec::setCodecForLocale(utf8Codec);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
